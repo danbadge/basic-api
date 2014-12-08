6337fd5dd08bf19034e1a612a339e08114183a8a
Daniel Watts <dwatts@eudwatts.local>
2014-12-08 13:51:51 +0000

    first test is passing

--- spec/status_spec.rb
+++ spec/status_spec.rb
@@ -4,7 +4,13 @@ require 'httparty'
 describe 'GET /status' do
 
   it 'should return 200 OK' do
-    response = HTTParty.get('http://localhost/status')
-    expect(true).to eq(false)
+    uri = 'http://localhost:4567/status'
+    puts 'GET ' + uri
+    response = HTTParty.get(uri)
+    puts 'Response'
+    puts 'Status code: ' + response.code.to_s
+    puts 'Body: ' + response.body
+    expect(response.code).to eq(200)
+    expect(response.body).to eq('200 OK')
   end
 end

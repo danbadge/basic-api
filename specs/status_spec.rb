07274127b6590f62ece52db191c71dbe791302ce
Daniel Watts <dwatts@eudwatts.local>
2014-12-08 13:31:01 +0000

    including httparty to make web requests

--- specs/status_spec.rb
+++ specs/status_spec.rb
@@ -1,9 +1,10 @@
 require 'rspec'
+require 'httparty'
 
 describe 'GET /status' do
 
   it 'should return 200 OK' do
-
+    response = HTTParty.get('http://localhost/status')
     expect(true).to eq(false)
   end
 end

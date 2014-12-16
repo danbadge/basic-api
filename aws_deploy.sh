9aeb995155dcff5898116fd6497596ffc1a200c3
Daniel Watts <dwatts@eudwatts.local>
2014-12-16 17:23:34 +0000

    push code up to s3

--- aws_deploy.sh
+++ aws_deploy.sh
@@ -1,10 +1,9 @@
-S3_BUCKET_NAME=`aws s3api list-buckets | grep -Eo 'deployment.*[^"]'`
-S3_BUCKET="s3://${S3_BUCKET_NAME}/"
 APP_VERSION=`git rev-parse --short HEAD`
 APP_NAME="basic-api"
 
 zip -r "${APP_NAME}-${APP_VERSION}.zip" . -x *.git* *.idea*
 
 pip install awscli
+S3_BUCKET=`aws s3api list-buckets | grep -Eo 'deployment.*[^"]'`
 
-aws something
+aws s3 cp ${APP_NAME}-${APP_VERSION}.zip s3://${S3_BUCKET}/${APP_NAME}-${APP_VERSION}.zip

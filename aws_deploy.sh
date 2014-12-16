c367a32ad9ad4832154a728d2a1d7cef0447efb3
Daniel Watts <dwatts@eudwatts.local>
2014-12-16 17:12:49 +0000

    working deploy file

--- aws_deploy.sh
+++ aws_deploy.sh
@@ -1,6 +1,7 @@
-export S3_BUCKET_NAME = `aws s3api list-buckets | grep -Eo 'deployment.*[^"]'`
-export S3_BUCKET = "s3://${S3_BUCKET_NAME}/"
-export APP_VERSION=`git rev-parse --short HEAD`
+S3_BUCKET_NAME=`aws s3api list-buckets | grep -Eo 'deployment.*[^"]'`
+S3_BUCKET="s3://${S3_BUCKET_NAME}/"
+APP_VERSION=`git rev-parse --short HEAD`
+APP_NAME="basic-api"
 
-zip -x *.git* -r "${APP_NAME}-${APP_VERSION}.zip" .
+zip -r "${APP_NAME}-${APP_VERSION}.zip" . -x *.git*
 pip install awscli

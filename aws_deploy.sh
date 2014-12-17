c70678cf58594dabe8bfdf4aa057c6a8d240de75
Daniel Watts <dwatts@eudwatts.local>
2014-12-17 10:58:21 +0000

    create an ec2 instance as part of the deploy

--- aws_deploy.sh
+++ aws_deploy.sh
@@ -1,9 +1,11 @@
 APP_VERSION=`git rev-parse --short HEAD`
-APP_NAME="basic-api"
+APP_NAME="basic-api-${APP_VERSION}"
 
-zip -r "${APP_NAME}-${APP_VERSION}.zip" . -x *.git* *.idea*
+zip -r "${APP_NAME}.zip" . -x *.git* *.idea*
 
 pip install awscli
 S3_BUCKET=`aws s3api list-buckets | grep -Eo 'deployment.*[^"]'`
 
-aws s3 cp ${APP_NAME}-${APP_VERSION}.zip s3://${S3_BUCKET}/${APP_NAME}-${APP_VERSION}.zip
+aws s3 cp ${APP_NAME}.zip s3://${S3_BUCKET}/${APP_NAME}.zip
+
+aws cloudformation create-stack --stack-name ${APP_NAME}

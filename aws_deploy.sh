dda30b69423f201ae7b02ee869d97458aec276af
Daniel Watts <dwatts@eudwatts.local>
2014-12-17 17:45:03 +0000

    no longer attempting to create new instances, instead stopping and starting them

--- aws_deploy.sh
+++ aws_deploy.sh
@@ -4,8 +4,15 @@ APP_NAME="basic-api-${APP_VERSION}"
 zip -r "${APP_NAME}.zip" . -x *.git* *.idea*
 
 pip install awscli
-S3_BUCKET=`aws s3api list-buckets | grep -Eo 'deployment.*[^"]'`
+pip install jq
 
-aws s3 cp ${APP_NAME}.zip s3://${S3_BUCKET}/${APP_NAME}.zip
+RUNNING_INSTANCE_IDS=`aws ec2 describe-instances --filters Name=instance-state-name,Values=running | jq --raw-output .Reservations[]?.Instances[]?.InstanceId`
+IDLE_INSTANCE_IDS=`aws ec2 describe-instances --filters Name=instance-state-name,Values=stopped | jq --raw-output .Reservations[]?.Instances[]?.InstanceId`
 
-aws cloudformation create-stack --stack-name ${APP_NAME} --template-body "file://./instances.json"
+aws ec2 start-instances --instance-ids ${IDLE_INSTANCE_IDS}
+
+aws ec2 stop-instances --instance-ids ${RUNNING_INSTANCE_IDS}
+
+#S3_BUCKET=`aws s3api list-buckets | grep -Eo 'deployment.*[^"]'`
+
+#aws s3 cp ${APP_NAME}.zip s3://${S3_BUCKET}/${APP_NAME}.zip 
